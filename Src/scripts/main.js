c929dd8a086f6b7efac8971f6282a6fbfa0422ff
Marina Marlag <marina.marlagkoutsou@gmail.com>
2018-11-26 16:51:53 +0200

    linked js file with index.html and made black menu sticky on scroll

--- Src/scripts/main.js
+++ Src/scripts/main.js
@@ -1,1 +1,19 @@
-console.log("Hello World from Main Javascript")
+var menu = document.querySelector('.u-head-part3');
+var menuPosition = menu.getBoundingClientRect();
+var placeholder = document.createElement('div');
+placeholder.style.width = menuPosition.width + 'px';
+placeholder.style.height = menuPosition.height + 'px';
+var isAdded = false;
+
+window.addEventListener('scroll', function() {
+    if (window.pageYOffset >= menuPosition.top && !isAdded) {
+        menu.classList.add('sticky');
+        menu.parentNode.appendChild(placeholder, menu);
+        isAdded = true;
+    } else if (window.pageYOffset < menuPosition.top && isAdded) {
+      console.log('remove')
+        menu.classList.remove('sticky');
+        menu.parentNode.removeChild(placeholder);
+        isAdded = false;
+    }
+});
