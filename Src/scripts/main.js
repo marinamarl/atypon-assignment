f126695f6c4831558db028319c850f01bb27aacb
Marina Marlag <marina.marlagkoutsou@gmail.com>
2018-11-28 23:56:10 +0200

    right sidebar slide in works

--- Src/scripts/main.js
+++ Src/scripts/main.js
@@ -26,6 +26,23 @@ function fixedHeader(){
   }
 }
 
+var sidebar = document.querySelector('.c-right-sidebar');
+var sideOpen = document.querySelector('.c-r-side-opener');
+var innerWrapper = document.querySelector('.l-inner-wrapper');
+sidebar.style.margin = '-20% 100%';
+function openNav() {
+    if (sidebar.style.margin == '-20% 100%'){
+        sidebar.style.margin = '-20% 0%';
+        sideOpen.style.margin = "10% -3%";
+        innerWrapper.classList.add('overlay');
+      } else {
+        sidebar.style.margin = '-20% 100%';
+        sideOpen.style.margin = "10% 3%";
+        innerWrapper.classList.remove('overlay');
+      }
+}
+
+
 // function fixedSidebar() {
 //   if( menu.classList.contains('fixed')){
 //     document.querySelector('.u-side-open').classList.add('fixed');
