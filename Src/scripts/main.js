5b491d915bf9e9d04b17ae4d96a9b2733fa72c64
Marina Marlag <marina.marlagkoutsou@gmail.com>
2018-11-29 05:02:35 +0200

    sidebar opener tweaked so first icon will open side and change styes

--- Src/scripts/main.js
+++ Src/scripts/main.js
@@ -9,10 +9,11 @@ placeholder.classList.add('fauxMenu');
 var isAdded = false;
 
 window.addEventListener('scroll', function() {
-  fixedHeader();
+  // fixedHeader();
+  myFunction();
 })
 
-function fixedHeader(){
+// function fixedHeader(){
 //   if (window.pageYOffset < menuPosition.top && isAdded) {
 //       menu.classList.remove('fixed');
 //       menu.parentNode.removeChild(placeholder);
@@ -23,19 +24,9 @@ function fixedHeader(){
 //       menu.parentNode.appendChild(placeholder, menu);
 //       isAdded = true;
 //   }
+// }
 // enable the previous and disable below, to remove slight 'jump' of menu on
 // scroll down, however it bugs out on mid page refresh
-var sticky = menu.offsetTop;
-
-if (window.pageYOffset >= sticky) {
-  menu.classList.add('fixed');
-} else {
-  menu.classList.remove('fixed');
-}
-
-}
-
-window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById('navbar');
 var sticky = navbar.offsetTop;
@@ -52,14 +43,18 @@ var sidebar = document.querySelector('.c-right-sidebar');
 var sideOpen = document.querySelector('.c-r-side-opener');
 var innerWrapper = document.querySelector('.l-inner-wrapper');
 sidebar.style.margin = '-20% 100%';
-function openNav() {
+function openNav(e) {
+var sideOpenDiv = document.querySelector('.c-r-side-opener div');
+  var sideOpenDivId = document.getElementById(e);
     if (sidebar.style.margin == '-20% 100%'){
         sidebar.style.margin = '-20% 0%';
         sideOpen.style.margin = "10% -3%";
+        sideOpenDivId.classList.add('clicked');
         innerWrapper.classList.add('overlay');
       } else {
         sidebar.style.margin = '-20% 100%';
         sideOpen.style.margin = "10% 3%";
+        sideOpenDiv.classList.remove('clicked');
         innerWrapper.classList.remove('overlay');
       }
 }
